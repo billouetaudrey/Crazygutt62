ba832a3eb465fd9e24b2f05b68e761cbe0a23bd3
Billou <32905881+billouetaudrey@users.noreply.github.com>
2025-09-19 17:44:22 +0200

    Update update_snake.php
    Ajout pret a la repro

--- public/update_snake.php
+++ public/update_snake.php
@@ -22,16 +22,21 @@ $weight = $_POST['weight'] !== '' ? (float)$_POST['weight'] : null;
 $default_meal_type = $_POST['default_meal_type'] !== '' ? $_POST['default_meal_type'] : null;
 $comment = trim($_POST['comment']);
 
+// **NOUVEAU** : Récupérer la valeur de la case à cocher
+// Si la case est cochée, la valeur sera '1', sinon '0'
+$ready_to_breed = isset($_POST['ready_to_breed']) ? 1 : 0;
+
 // Préparer la requête SQL pour la mise à jour
 // L'ordre des colonnes ici doit correspondre à l'ordre des variables ci-dessous
 $stmt = $pdo->prepare("
     UPDATE snakes
-    SET name = ?, sex = ?, morph = ?, birth_year = ?, weight = ?, default_meal_type = ?, comment = ?
+    SET name = ?, sex = ?, morph = ?, birth_year = ?, weight = ?, default_meal_type = ?, comment = ?, ready_to_breed = ?
     WHERE id = ?
 ");
 
 // Exécuter la requête
 // L'ordre des variables DOIT correspondre à l'ordre des ? dans la requête
+// **NOUVEAU** : Ajout de la variable $ready_to_breed avant l'ID
 $stmt->execute([
     $name,
     $sex,
@@ -40,6 +45,7 @@ $stmt->execute([
     $weight,
     $default_meal_type,
     $comment,
+    $ready_to_breed, // Nouvelle variable
     $id
 ]);
 
